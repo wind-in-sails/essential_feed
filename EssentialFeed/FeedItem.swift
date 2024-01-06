6cf78dcd6d88b571dc2247aa49c6262e049401a9
Sergey Kudryavtsev <sergey.developer@bk.ru>
2024-01-05 19:30:29 -0600

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sergey Kudryavtsev on 05.01.2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
